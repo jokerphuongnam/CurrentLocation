6f96daa0af64d471571284b8151a0478e2f34a7a
Phạm P. Nam <phuongnamp99@gmail.com>
2021-02-23 16:29:27 +0700

    Thêm singleton vào LiveUseCase

--- app/src/main/java/pnam/currentlocation/model/usecase/live/LiveLocationUseCase.kt
+++ app/src/main/java/pnam/currentlocation/model/usecase/live/LiveLocationUseCase.kt
@@ -3,7 +3,9 @@ package pnam.currentlocation.model.usecase.live
 import kotlinx.coroutines.flow.Flow
 import pnam.currentlocation.model.database.domain.Location
 import pnam.currentlocation.model.repository.location.LocationRepository
+import javax.inject.Singleton
 
+@Singleton
 interface LiveLocationUseCase {
     val locationRepository: LocationRepository
     fun getLiveLocation(): Flow<Location>
